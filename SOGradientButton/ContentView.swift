bc0311376a53a16fa322186aaef1349ab317cb56
Wang Li <aliliwvip@gmail.com>
2021-08-10 13:57:11 +0800

    添加button点击⌚️

--- SOGradientButton/ContentView.swift
+++ SOGradientButton/ContentView.swift
@@ -11,8 +11,18 @@ struct ContentView: View {
     var brandGradient = Gradient(colors: [Color(.systemTeal), Color(.systemPurple)])
     var body: some View {
         VStack(spacing: 60){
-            SOGradientButton(title: "Linear Gradient", brandGradient: brandGradient, size: CGSize(width: 280, height: 50), start: .leading, end: .trailing, backgroundType: .linear, foregroundColor: .white)
-            SOGradientButton(title: "Linear Border Gradient", brandGradient: brandGradient, size: CGSize(width: 280, height: 50), start: .leading, end: .trailing, backgroundType: .clear, foregroundColor: .black, borderWidth: 5)
+            Button {
+                print("Linear Gradient")
+            } label: {
+                SOGradientView(title: "Linear Gradient", brandGradient: brandGradient, size: CGSize(width: 280, height: 50), start: .leading, end: .trailing, backgroundType: .linear, foregroundColor: .white)
+            }
+
+            Button {
+                print("Linear Border Gradient")
+            } label: {
+                SOGradientView(title: "Linear Border Gradient", brandGradient: brandGradient, size: CGSize(width: 280, height: 50), start: .leading, end: .trailing, backgroundType: .clear, foregroundColor: .black, borderWidth: 5)
+            }
+            
         }
     }
 }
@@ -23,7 +33,7 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct SOGradientButton: View {
+struct SOGradientView: View {
     let title: String
     let brandGradient: Gradient
     let size: CGSize
@@ -32,29 +42,28 @@ struct SOGradientButton: View {
     let backgroundType: SOGradientBackGroundType
     let foregroundColor: Color?
     var borderWidth: CGFloat? = 0
+    
     var body: some View {
-        Button {
-            print("Linear Gradient Button Tapped")
-        } label: {
-            switch backgroundType {
-            case .linear:
-                Text(title)
-                    .bold()
-                    .frame(width: size.width, height: size.height)
-                    .foregroundColor(foregroundColor)
-                    .background(LinearGradient(gradient: brandGradient, startPoint: start, endPoint: end))
-                    .clipShape(Capsule())
-            case .clear:
-                Text(title)
-                    .bold()
-                    .frame(width: size.width, height: size.height)
-                    .foregroundColor(foregroundColor)
-                    .background(Color.clear)
-                    .overlay(Capsule().stroke(LinearGradient(gradient: brandGradient, startPoint: .leading, endPoint: .trailing), lineWidth: borderWidth!))
-            }
-            
+        switch backgroundType {
+        case .linear:
+            Text(title)
+                .bold()
+                .frame(width: size.width, height: size.height)
+                .foregroundColor(foregroundColor)
+                .background(LinearGradient(gradient: brandGradient, startPoint: start, endPoint: end))
+                .clipShape(Capsule())
+        case .clear:
+            Text(title)
+                .bold()
+                .frame(width: size.width, height: size.height)
+                .foregroundColor(foregroundColor)
+                .background(Color.clear)
+                .overlay(Capsule().stroke(LinearGradient(gradient: brandGradient, startPoint: .leading, endPoint: .trailing), lineWidth: borderWidth!))
         }
+        
     }
+    
+  
 }
 
 enum SOGradientBackGroundType {
